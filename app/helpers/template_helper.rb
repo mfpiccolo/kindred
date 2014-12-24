bc6892600178f68e37088a94413efdb89328830c
Mike Piccolo <mfpiccolo@gmail.com>
2014-12-24 00:22:08 -0800

    Allow symbol to be passed

--- app/helpers/template_helper.rb
+++ app/helpers/template_helper.rb
@@ -19,27 +19,27 @@ module TemplateHelper
   end
 
   def target(object)
-    "data-target data-target-uuid=" + object.try(:uuid).to_s
+    "data-target data-target-uuid=" + k_try(object, :uuid).to_s
   end
 
   def k_content_tag(element, attribute = nil, object = nil, content_or_options_with_block = nil, options = {}, escape = true, &block)
-    content_tag(element, nil, options.merge({data: { attr: attribute, k_uuid: object.try(:uuid), val: ""} }))
+    content_tag(element, nil, options.merge({data: { attr: attribute, k_uuid: k_try(object, :uuid), val: ""} }))
   end
 
   def k_hidden_field_tag(name, value=nil, object=nil, delegate_to=nil, options = {})
-    hidden_field_tag name, value, options.merge({data: { attr: name, k_uuid: object.try(:uuid), val: value } })
+    hidden_field_tag name, value, options.merge({data: { attr: name, k_uuid: k_try(object, :uuid), val: value } })
   end
 
   def k_text_field_tag(object, attribute, options={})
-    text_field_tag attribute, nil, options.merge({data: { attr: attribute, k_uuid: object.try(:uuid), val: "" } })
+    text_field_tag attribute, nil, options.merge({data: { attr: attribute, k_uuid: k_try(object, :uuid), val: "" } })
   end
 
   def k_check_box_tag(object, name, value=nil, checked = false, options = {})
-    check_box_tag name, value, checked, options.merge({data: { attr: name, k_uuid: object.try(:uuid), val: ""} })
+    check_box_tag name, value, checked, options.merge({data: { attr: name, k_uuid: k_try(object, :uuid), val: ""} })
   end
 
   def k_select_tag(object, name, option_tags = nil, options = {})
-    select_tag name, option_tags, options.merge(data: { attr: name, k_uuid: object.try(:uuid), val: "" })
+    select_tag name, option_tags, options.merge(data: { attr: name, k_uuid: k_try(object, :uuid), val: "" })
   end
 
   def error_for(object, attribute)
@@ -48,6 +48,12 @@ module TemplateHelper
 
   def kindred_model_data
     "<div data-kindred-model style='display:none;'></div>".html_safe
+  end
+
+  def k_try(object, method)
+    unless object.is_a?(Symbol)
+      object.try method
+    end
   end
 
 end
